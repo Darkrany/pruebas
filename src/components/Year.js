5b71d27981a5dd6908b332288b95597d52f5e904
Satgoltratec <116006255+Satgoltratec@users.noreply.github.com>
2023-03-06 17:45:36 +0100

    Initial commit

---
+++ src/components/Year.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import { useState } from 'react';
+import PropTypes from 'prop-types';
+
+
+
+export  function Year() {
+
+  const currentYear = new Date().getFullYear();
+  
+  const [YearCounter, setYear] = useState(currentYear)
+  
+  const BackYear = () => {
+     
+    setYear(YearCounter-1);
+  }
+  
+  const NextYear = () =>{
+
+    setYear(YearCounter+1);
+  }
+ 
+  return (
+    <div>
+        
+        <p>{YearCounter}</p>
+          <button onClick={BackYear} >Año Anterior</button>
+          <br/>
+          <button onClick={NextYear}>Siguiente Año</button>
+          <br/>
+          <input onChange={e => setYear(e.target.value)}/>
+
+      </div>
+  )
+
+
+
+}
+
+Year.propTypes = {
+    currentYear: PropTypes.number.isRequired
+}
